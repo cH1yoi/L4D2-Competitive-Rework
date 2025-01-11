16ba888270a744e450a75838ca22dad973f922ff
cH1yoi <chiyoo1@outlook.com>
2025-01-11 11:49:44 +0800

    u

---
+++ requirements.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+clear
+function install_dependencies() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        OS=$ID
+    else
+        echo "Unsupported operating system!"
+    fi
+
+    echo "Installing dependencies..."
+    
+    if [ "$OS" == "ubuntu" ]; then
+        sudo dpkg --add-architecture i386
+        sudo apt update
+        sudo apt install -y lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 zlib1g:i386 
+    elif [ "$OS" == "debian" ]; then
+        sudo dpkg --add-architecture i386
+        sudo apt update
+        sudo apt install -y lib32gcc-s1 lib32stdc++6 zlib1g
+    else
+        echo "Unsupported operating system!"
+    fi
+
+    sudo apt install -y wget curl screen unzip zip git
+}
+
+install_dependencies
